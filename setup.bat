d3365ecac41f17856123f267d14b53f3cba2a7fb
Syed Mustafa Ahmad <syedmustafaahmad@gmail.com>
2022-07-12 20:36:28 +0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,5 +2,10 @@
 mkdir WAFER
 cd WAFER
 curl https://github.com/WAFERed/BlueEngine/archive/refs/heads/main.zip -O BlueEngine.zip
-cd BlueEngine
+tar -xf BlueEngine.zip
+cd BlueEngine.zip
 curl https://github.com/WAFERed/WaterBucket/archive/refs/heads/main.zip -O WaterBucket.zip
+tar -xf WaterBucket.zip
+rm WaterBucket.zip
+cd ..
+rm BlueEngine.zip
