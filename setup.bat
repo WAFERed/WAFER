352f6aaf499916df081ff75ee66fb3e8d7e960a8
Syed Mustafa Ahmad <syedmustafaahmad@gmail.com>
2022-07-12 20:22:24 +0500

    Windows Support

---
+++ setup.bat
@@ -0,0 +1,6 @@
+@echo off
+mkdir WAFER
+cd WAFER
+curl https://github.com/WAFERed/BlueEngine/archive/refs/heads/main.zip -O BlueEngine.zip
+cd BlueEngine
+curl https://github.com/WAFERed/WaterBucket/archive/refs/heads/main.zip -O WaterBucket.zip
